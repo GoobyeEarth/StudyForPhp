dacd15eb7768bbbeb50281018132a62398112394
GoobyeEarth <ra4chrome.ctrl@gmail.com>
2016-02-20 02:52:40 -0500

    #05

--- php_database/index.php
+++ php_database/index.php
@@ -11,9 +11,10 @@ try {
   $db = new PDO(PDO_DSN, DB_USERNAME, DB_PASSWORD);
   $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-  $db->exec("insert into users (name, score) values ('taguchi', 55)");
-  echo "user added!";
-
+  
+  $stmt = $db->prepare("insert into users (name, score) values (?, ?)");
+  $stmt->execute(['taguchi', 44]);
+  echo "inserted: " . $db->lastInsertId();
 
 
   //disconnect
